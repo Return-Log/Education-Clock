d1a88116fdfa7073c404b7a517bcc3f590648fd6
Return-Log <chnlmr@outlook.com>
2024-06-09 01:54:11 +0800

    -重构天气模块 -更改自动关机窗口显示大小

--- timer_shut_down.py
+++ timer_shut_down.py
@@ -12,18 +12,25 @@ import os
 
 
 class CountdownDialog(QDialog):
+    """
+    关机倒计时对话框类
+    """
+
     def __init__(self, parent=None):
+        """
+        初始化倒计时对话框
+        """
         super().__init__(parent)
         self.setWindowTitle('关机倒计时')
         self.setWindowFlag(Qt.WindowStaysOnTopHint)
         self.countdown_label = QLabel(self)
         self.countdown_label.setAlignment(Qt.AlignCenter)
-        self.countdown_label.setStyleSheet("font-size: 40px;")
+        self.countdown_label.setStyleSheet("font-size: 60px; font-weight: bold; font-family: 'SimHei'; color: #003399;")
         self.cancel_shutdown_button = QPushButton('取消关机', self)
-        self.cancel_shutdown_button.setStyleSheet("background-color: red; color: black;")
+        self.cancel_shutdown_button.setStyleSheet("font-size: 70px; font-weight: bold; font-family: 'SimHei'; background-color: #CC0033; color: #000000;")
         self.cancel_shutdown_button.clicked.connect(self.cancel_shutdown)
         self.shutdown_now_button = QPushButton('立即关机', self)
-        self.shutdown_now_button.setStyleSheet("background-color: green; color: black;")
+        self.shutdown_now_button.setStyleSheet("font-size: 70px; font-weight: bold; font-family: 'SimHei'; background-color: #99CC00; color: #000000;")
         self.shutdown_now_button.clicked.connect(self.shutdown_now)
 
         layout = QVBoxLayout(self)
@@ -39,22 +46,38 @@ class CountdownDialog(QDialog):
         self.update_countdown()
 
     def update_countdown(self):
+        """
+        更新倒计时标签的文本
+        """
         self.countdown_label.setText(f"关机倒计时：{self.countdown_seconds}秒")
         if self.countdown_seconds == 0:
             self.shutdown_now()
         self.countdown_seconds -= 1
 
     def cancel_shutdown(self):
+        """
+        取消关机操作
+        """
         self.timer.stop()
         self.reject()
 
     def shutdown_now(self):
+        """
+        立即关机
+        """
         self.timer.stop()
         self.accept()
 
 
 class ShutdownTimerApp(QWidget):
+    """
+    关机计时器应用程序类
+    """
+
     def __init__(self):
+        """
+        初始化关机计时器应用程序
+        """
         super().__init__()
         self.shutdown_times = self.read_shutdown_times()
         self.initUI()
@@ -67,6 +90,9 @@ class ShutdownTimerApp(QWidget):
         self.delay_timer.timeout.connect(self.reset_countdown_active)
 
     def read_shutdown_times(self):
+        """
+        读取并返回关机时间配置
+        """
         try:
             with open('data/closetime.json5', 'r', encoding='utf-8') as file:
                 data = json5.load(file)
@@ -75,18 +101,19 @@ class ShutdownTimerApp(QWidget):
             return {}
 
     def initUI(self):
+        """
+        初始化用户界面
+        """
         self.setWindowTitle('关机倒计时')
         self.resize(300, 150)
         self.center()
 
-        self.setStyleSheet("""
-            QPushButton {
-                font-size: 32px;
-                font-weight: bold;
-            }
-        """)
+        self.setStyleSheet("background-color: #7c8577;")  # 设置背景色和字体颜色
 
     def center(self):
+        """
+        将窗口居中显示
+        """
         screen_geometry = QDesktopWidget().screenGeometry()
         window_geometry = self.frameGeometry()
         x = (screen_geometry.width() - window_geometry.width()) // 2
@@ -94,6 +121,9 @@ class ShutdownTimerApp(QWidget):
         self.move(x, y)
 
     def check_shutdown_time(self):
+        """
+        检查是否应该启动关机倒计时
+        """
         if self.countdown_active:
             return
 
@@ -117,13 +147,17 @@ class ShutdownTimerApp(QWidget):
 
             for shutdown_time in shutdown_times_today:
                 shutdown_time = QTime.fromString(shutdown_time, 'hh:mm')
-                if current_time.addSecs(-60) <= shutdown_time <= current_time.addSecs(60):
+                # 检查当前时间是否在关机时间的前后1秒内，以确定是否需要关机
+                if current_time.addSecs(-1200) <= shutdown_time <= current_time.addSecs(1200):
                     self.show_countdown_dialog()
                     return
         except Exception as e:
             pass  # 处理异常（如有必要，您可以在此处添加日志记录）
 
     def show_countdown_dialog(self):
+        """
+        显示关机倒计时对话框
+        """
         self.countdown_active = True
         self.countdown_dialog = CountdownDialog(self)
         if self.countdown_dialog.exec_() == QDialog.Accepted:
@@ -132,16 +166,28 @@ class ShutdownTimerApp(QWidget):
             self.start_delay_timer()
 
     def shutdown(self):
+        """
+        执行关机操作
+        """
         os.system('shutdown /s /t 1')
         sys.exit()
 
     def start_delay_timer(self):
+        """
+        启动延迟计时器，用于在倒计时对话框被取消后等待一段时间再允许新的倒计时启动
+        """
         self.delay_timer.start(120000)  # 2分钟延迟
 
     def reset_countdown_active(self):
+        """
+        重置倒计时活动状态
+        """
         self.countdown_active = False
 
     def stop(self):
+        """
+        停止计时器
+        """
         self.timer.stop()
         self.delay_timer.stop()
 
--- weather.py
+++ weather.py
@@ -8,10 +8,8 @@ import sys
 from datetime import datetime
 import requests
 from PyQt5.QtCore import Qt, QTimer, QPoint, QSettings, QThread, pyqtSignal
-from PyQt5.QtGui import QFont
-from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QDialog, QLineEdit, QMessageBox
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
-
+from PyQt5.QtGui import QFont, QPixmap
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QDialog, QLineEdit, QMessageBox, QPushButton
 
 class WeatherThread(QThread):
     weather_data_fetched = pyqtSignal(dict)
@@ -23,72 +21,58 @@ class WeatherThread(QThread):
         self.longitude = longitude
 
     def run(self):
-        url = f"http://api.openweathermap.org/data/2.5/forecast?lat={self.latitude}&lon={self.longitude}&appid={self.api_key}"
+        url = f"http://api.openweathermap.org/data/2.5/forecast?lat={self.latitude}&lon={self.longitude}&appid={self.api_key}&units=metric"
         response = requests.get(url)
         data = response.json()
         self.weather_data_fetched.emit(data)
 
-
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.api_key = self.load_api_key()  # 加载 OpenWeather API 密钥
-        self.latitude, self.longitude = self.load_location()  # 加载位置信息
-        self.setStyleSheet("background-color: black; color: white;")  # 设置窗口样式
-        self.init_ui()  # 初始化用户界面
-        self.restore_settings()  # 恢复窗口位置设置
+        self.api_key = self.load_api_key()
+        self.latitude, self.longitude = self.load_location()
+        self.init_ui()
+        self.restore_settings()
 
-    # 加载 OpenWeather API 密钥
     def load_api_key(self):
         with open("data/OpenWeather-API.txt", "r") as file:
             return file.read().strip()
 
-    # 加载位置信息
     def load_location(self):
         with open("data/location.txt", "r") as file:
             location = file.read().strip()
             latitude, longitude = location.split(',')
             return float(latitude), float(longitude)
 
-    # 初始化用户界面
     def init_ui(self):
         self.setWindowTitle("天气预报")
-        self.setWindowFlags(Qt.FramelessWindowHint)  # 设置窗口无边框
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint)
         layout = QVBoxLayout()
 
-        # 第一行显示当前天气温度天气描述及风速，红色
-        self.current_weather_label = QLabel("当前天气", self)
-        self.current_weather_label.setFont(QFont("Arial", 24))
-        self.current_weather_label.setStyleSheet("color: red")
-        layout.addWidget(self.current_weather_label)
+        self.current_weather_layout = QHBoxLayout()
+        self.current_weather_icon_label = QLabel("", self)
+        self.current_weather_icon_label.setFixedSize(100, 100)
+        self.current_weather_layout.addWidget(self.current_weather_icon_label)
 
-        # 第三行显示未来5天每天天气，黄色
-        self.future_5_days_label = QLabel("未来5天天气", self)
-        self.future_5_days_label.setFont(QFont("Arial", 12))
-        self.future_5_days_label.setStyleSheet("color: yellow")
-        layout.addWidget(self.future_5_days_label)
+        self.current_temp_label = QLabel("", self)
+        self.current_temp_label.setFont(QFont("Arial", 19))
+        self.current_weather_layout.addWidget(self.current_temp_label)
 
-        # 最后一行显示更新时间，绿色
-        self.update_time_label = QLabel("更新时间：", self)
-        self.update_time_label.setFont(QFont("Arial", 12))
-        self.update_time_label.setStyleSheet("color: green")
-        layout.addWidget(self.update_time_label)
+        layout.addLayout(self.current_weather_layout)
+        self.forecast_layout = QHBoxLayout()
+        layout.addLayout(self.forecast_layout)
 
         self.setLayout(layout)
+        self.resize(300, 200)
         self.show()
 
-        # 双击窗口弹出设置窗口
         self.mouseDoubleClickEvent = self.open_location_dialog
-
-        # 启动时自动刷新天气
         self.start_weather_thread()
 
-        # 定时器，每隔 5分钟刷新一次
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.start_weather_thread)
-        self.timer.start(300000)  # 5分钟 * 60秒 * 1000毫秒
+        self.timer.start(300000)
 
-    # 双击窗口弹出设置窗口
     def open_location_dialog(self, event):
         location_dialog = LocationDialog(self)
         location_dialog.exec_()
@@ -106,12 +90,10 @@ class WeatherApp(QWidget):
         if settings.contains("geometry"):
             self.restoreGeometry(settings.value("geometry"))
 
-    # 处理鼠标按下事件，用于拖动窗口
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             self.oldPos = event.globalPos()
 
-    # 处理鼠标移动事件，用于拖动窗口
     def mouseMoveEvent(self, event):
         delta = QPoint(event.globalPos() - self.oldPos)
         self.move(self.x() + delta.x(), self.y() + delta.y())
@@ -122,88 +104,90 @@ class WeatherApp(QWidget):
         self.weather_thread.weather_data_fetched.connect(self.display_weather)
         self.weather_thread.start()
 
-    # 显示天气信息
     def display_weather(self, weather_data):
         if weather_data is None:
             return
 
         try:
-            current_temp = weather_data['list'][0]['main'].get('temp', -999)
-            humidity = weather_data['list'][0]['main'].get('humidity', -999)
-            wind_speed = weather_data['list'][0]['wind'].get('speed', -999)
-            weather_main = weather_data['list'][0]['weather'][0].get('main', 'N/A')
+            current_weather = weather_data['list'][0]
+            current_temp = current_weather['main'].get('temp', -999)
+            humidity = current_weather['main'].get('humidity', -999)
+            wind_speed = current_weather['wind'].get('speed', -999)
+            weather_icon = current_weather['weather'][0].get('icon', '01d')
+            weather_desc = current_weather['weather'][0].get('main', '')
 
-            if -100 <= current_temp <= 900:  # 检查温度是否在合理范围内
-                current_temp_str = f"{current_temp - 273.15:.2f}"  # 转换为摄氏度并保留两位小数
+            if -100 <= current_temp <= 900:
+                current_temp_str = f"{current_temp:.0f}°C"
             else:
                 current_temp_str = 'N/A'
+            self.current_temp_label.setText(f"{current_temp_str} | 湿度: {humidity}% | 风速: {wind_speed} m/s")
 
-            # 天气描述中英文对照字典
-            weather_desc_dict = {
-                'Clear': '晴',
-                'Clouds': '多云',
-                'Rain': '雨',
-                'Drizzle': '细雨',
-                'Thunderstorm': '雷暴',
-                'Snow': '雪',
-                'Mist': '雾',
-                'Smoke': '烟',
-                'Haze': '霾',
-                'Dust': '尘',
-                'Fog': '雾',
-                'Sand': '沙',
-                'Ash': '灰',
-                'Squall': '狂风',
-                'Tornado': '龙卷风',
-            }
-            weather_main_cn = weather_desc_dict.get(weather_main, '未知')
+            icon_url = f"http://openweathermap.org/img/wn/{weather_icon}@2x.png"
+            weather_icon_pixmap = QPixmap()
+            weather_icon_pixmap.loadFromData(requests.get(icon_url).content)
+            self.current_weather_icon_label.setPixmap(weather_icon_pixmap.scaled(100, 100, Qt.KeepAspectRatio))
 
-            current_weather_str = f"当前温度：{current_temp_str} ℃，{weather_main_cn}   湿度：{humidity}%   风速：{wind_speed} m/s"
-            self.current_weather_label.setText(current_weather_str)
+            self.update_background()
 
-            # 获取未来5天每天的总体天气情况
             future_5_days_data = weather_data['list']
-            daily_weather = {}  # 存储每天的天气情况
+            daily_weather = {}
+
             for day_weather in future_5_days_data:
-                date_time = day_weather['dt_txt']  # 提取日期时间
-                date = date_time.split()[0].split('-')[1:]  # 提取日期部分并去掉年份
-                date_str = '-'.join(date)
-                time = date_time.split()[1].split(':')[0]  # 提取小时部分
+                date_time = day_weather['dt_txt']
+                date = date_time.split()[0]
+                time = date_time.split()[1]
                 weather_desc = day_weather['weather'][0].get('main', 'N/A')
-                weather_desc_chinese = weather_desc_dict.get(weather_desc, '未知')
+                weather_icon = day_weather['weather'][0].get('icon', '01d')
                 max_temp = day_weather['main'].get('temp_max', -999)
                 min_temp = day_weather['main'].get('temp_min', -999)
 
-                # 更新或添加每天的天气情况
-                if date_str in daily_weather:
-                    daily_weather[date_str]['weather'].append(f"{time}: {weather_desc_chinese}")
-                    daily_weather[date_str]['max_temp'] = max(max_temp, daily_weather[date_str]['max_temp'])
-                    daily_weather[date_str]['min_temp'] = min(min_temp, daily_weather[date_str]['min_temp'])
+                if date in daily_weather:
+                    daily_weather[date]['max_temp'] = max(max_temp, daily_weather[date]['max_temp'])
+                    daily_weather[date]['min_temp'] = min(min_temp, daily_weather[date]['min_temp'])
                 else:
-                    daily_weather[date_str] = {'weather': [f"{time}: {weather_desc_chinese}"], 'max_temp': max_temp,
-                                               'min_temp': min_temp}
+                    daily_weather[date] = {'weather': weather_desc, 'icon': weather_icon, 'max_temp': max_temp, 'min_temp': min_temp}
 
-            future_5_days_str = ""
+            for i in reversed(range(self.forecast_layout.count())):
+                self.forecast_layout.itemAt(i).widget().setParent(None)
+
             for date, weather_info in daily_weather.items():
-                max_temp_celsius = round(weather_info['max_temp'] - 273.15, 2) if weather_info[
-                                                                                      'max_temp'] != -999 else 'N/A'
-                min_temp_celsius = round(weather_info['min_temp'] - 273.15, 2) if weather_info[
-                                                                                      'min_temp'] != -999 else 'N/A'
-                future_5_days_str += f"{date}：{', '.join(weather_info['weather'])}，最高温度：{max_temp_celsius} ℃，最低温度：{min_temp_celsius} ℃\n"
-            self.future_5_days_label.setText("未来5天天气：\n" + future_5_days_str)
+                forecast_widget = QWidget()
+                forecast_layout = QVBoxLayout()
 
-            # 更新数据更新时间
-            self.update_time_label.setText(
-                "更新时间：" + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + "   数据来源：OpenWeather")
+                display_date = '-'.join(date.split('-')[1:])
+                date_label = QLabel(display_date, self)
+                date_label.setFont(QFont("Arial", 12))
+                date_label.setAlignment(Qt.AlignCenter)
+                forecast_layout.addWidget(date_label)
 
-            # 调整窗口大小以适应内容
+                weather_icon_label = QLabel(self)
+                icon_url = f"http://openweathermap.org/img/wn/{weather_info['icon']}@2x.png"
+                weather_icon_pixmap = QPixmap()
+                weather_icon_pixmap.loadFromData(requests.get(icon_url).content)
+                weather_icon_label.setPixmap(weather_icon_pixmap.scaled(75, 75, Qt.KeepAspectRatio))
+                weather_icon_label.setAlignment(Qt.AlignCenter)
+                forecast_layout.addWidget(weather_icon_label)
+
+                temp_label = QLabel(f"{weather_info['max_temp']:.0f}°C\n{weather_info['min_temp']:.0f}°C", self)
+                temp_label.setFont(QFont("Arial", 12))
+                temp_label.setAlignment(Qt.AlignCenter)
+                forecast_layout.addWidget(temp_label)
+
+                forecast_widget.setLayout(forecast_layout)
+                self.forecast_layout.addWidget(forecast_widget)
+
             self.adjustSize()
 
         except Exception as e:
             print("Error:", e)
 
+    def update_background(self):
+        current_hour = datetime.now().hour
+        if 6 <= current_hour < 18:  # 白天时间段
+            self.setStyleSheet("background-color: lightblue; color: black;")
+        else:  # 晚上时间段
+            self.setStyleSheet("background-color: darkblue; color: white;")
 
-# 设置位置对话框
 class LocationDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -211,7 +195,6 @@ class LocationDialog(QDialog):
         self.setWindowFlags(Qt.Dialog)
         self.init_ui()
 
-    # 初始化设置窗口界面
     def init_ui(self):
         layout = QVBoxLayout()
 
@@ -231,7 +214,6 @@ class LocationDialog(QDialog):
 
         self.setLayout(layout)
 
-    # 检查输入的坐标是否有效
     def check_coordinate_validity(self, coordinate):
         try:
             float(coordinate)
@@ -239,7 +221,6 @@ class LocationDialog(QDialog):
         except ValueError:
             return False
 
-    # 保存位置信息到文件中
     def save_location(self):
         latitude = self.latitude_input.text()
         longitude = self.longitude_input.text()
@@ -256,11 +237,10 @@ class LocationDialog(QDialog):
                 file.write(f"{latitude},{longitude}")
             self.parent().latitude = latitude
             self.parent().longitude = longitude
-            self.parent().start_weather_thread()  # 更新位置后重新获取天气数据
+            self.parent().start_weather_thread()
             self.close()
         else:
-            QMessageBox.warning(self, "错误", "请输入合法的纬度和经度范围。")
-
+            QMessageBox.warning(self, "错误", "请输入有效范围内的纬度和经度。")
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
