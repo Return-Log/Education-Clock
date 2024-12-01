e0ba1a5d97fd7c68e636d878a27c7c54b124fbea
Return-Log <chnlmr@outlook.com>
2024-12-01 13:32:45 +0800

    v3.5 bug修复

--- settings_window.py
+++ settings_window.py
@@ -12,9 +12,6 @@ from PyQt6.QtWidgets import (
 from PyQt6.uic import loadUi
 from PyQt6.QtCore import QTimer, QDateTime, QDate
 
-# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-
 class SettingsWindow(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
